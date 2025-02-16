2f4bf85fb28177d089af6c4713b960ac490f3657
BarryDong <donghao@bwcj.com>
2025-02-16 16:50:54 +0800

    路由调整

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ScratchCard from '../views/ScratchCard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +12,7 @@ const router = createRouter({
     {
       path: '/scratch-card',
       name: 'scratchCard',
-      component: ScratchCard,
+      component: () => import('../views/ScratchCard.vue'),
     },
   ],
 })
